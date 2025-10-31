3587f300404943322c4b181a512355b9f50b1a8d
Alejandro Moreno <alejandromogil@gmail.com>
2025-10-30 22:49:15 -0500

    feat: add Swagger setup for API documentation and configure global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('PetCare API')
+    .setDescription('API documentation for the PetCare application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = new ConfigService();
 
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
 
-  await app.listen(3000);
+  setupSwagger(app);
+
+  await app.listen(configService.get('PORT') || 3000);
+  const url = await app.getUrl();
+  Logger.log(`Application is running on: ${url}`, 'NestApplication');
 }
+
 bootstrap();
