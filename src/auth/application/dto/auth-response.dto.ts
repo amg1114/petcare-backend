3c400c6f51c6f7e5c59e46e2e4e939fbf17b30d5
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-02 17:00:33 -0500

    feat: enhance authentication flow with JWT payload interface and user mapping

--- src/auth/application/dto/auth-response.dto.ts
+++ src/auth/application/dto/auth-response.dto.ts
@@ -1,6 +1,11 @@
-import { ApiSchema } from '@nestjs/swagger';
+import { ApiProperty, ApiSchema } from '@nestjs/swagger';
+import { UserResponseDTO } from 'src/users/application/dto/user-response.dto';
 
 @ApiSchema({ description: 'Data transfer object for auth request responses' })
 export class AuthResponseDTO {
+  @ApiProperty({ description: 'JWT Token to be authenticated.' })
   access_token: string;
+
+  @ApiProperty({ description: 'Data transfer object for authenticated user.' })
+  user: UserResponseDTO;
 }
