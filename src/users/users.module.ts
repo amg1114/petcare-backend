116ebe9921c6e778929fa746c72e1ce73c27ae3c
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-02 17:50:57 -0500

    refactor: remove CreateUserDTO and CreateUserUseCase from the user module and controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserORMEntity } from './infrastructure/orm/user.orm-entity';
 import { UsersController } from './presentation/controllers/users.controller';
-import { CreateUserUseCase } from './application/use-cases/create-user.usecase';
 import { UserORMRepository } from './infrastructure/repositories/user.orm-repository';
 import { UpdateUserUseCase } from './application/use-cases/update-user.usecase';
 import { DeleteUserUseCase } from './application/use-cases/delete-user.usecase';
@@ -15,12 +14,16 @@ import { GetUserUseCase } from './application/use-cases/get-user.usecase';
       provide: 'UserRepository',
       useClass: UserORMRepository,
     },
-    CreateUserUseCase,
     UpdateUserUseCase,
     DeleteUserUseCase,
     GetUserUseCase,
   ],
   controllers: [UsersController],
-  exports: [],
+  exports: [
+    {
+      provide: 'UserRepository',
+      useClass: UserORMRepository,
+    },
+  ],
 })
 export class UsersModule {}
