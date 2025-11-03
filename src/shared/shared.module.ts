e970edf5e96f90d08656cf4f535ed6716bfcdf4b
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-02 20:12:48 -0500

    feat: implement PasswordService with hashing and comparison methods

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PasswordService } from './infraestructure/services/password.service';
+
+@Module({
+  providers: [
+    {
+      provide: 'PasswordService',
+      useClass: PasswordService,
+    },
+  ],
+  exports: ['PasswordService'],
+})
+export class SharedModule {}
