c9c80cd952517b84fc7147076848270f55752809
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-02 20:12:48 -0500

    feat: implement PasswordService with hashing and comparison methods

---
+++ src/shared/infraestructure/services/password.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { IPasswordService } from '../../domain/services/password.service.interface';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class PasswordService implements IPasswordService {
+  private readonly SALT_ROUNDS = 10;
+
+  hash(password: string): Promise<string> {
+    return bcrypt.hash(password, this.SALT_ROUNDS);
+  }
+
+  compare(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
