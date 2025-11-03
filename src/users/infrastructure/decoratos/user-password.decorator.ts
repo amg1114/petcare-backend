ebd34d3195abf5df43065e66a80874d3cf3f39fc
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-02 20:22:48 -0500

    feat: implement custom IsUserPassword decorator for password validation in LoginDTO and RegisterDTO

---
+++ src/users/infrastructure/decoratos/user-password.decorator.ts
@@ -0,0 +1,20 @@
+import { applyDecorators } from '@nestjs/common';
+import { IsStrongPassword } from 'class-validator';
+
+export function IsUserPassword() {
+  return applyDecorators(
+    IsStrongPassword(
+      {
+        minLength: 8,
+        minNumbers: 1,
+        minLowercase: 1,
+        minUppercase: 1,
+        minSymbols: 0,
+      },
+      {
+        message:
+          'The password must have a minimum of 8 characters, at least one uppercase letter, one lowercase letter and one number.',
+      },
+    ),
+  );
+}
