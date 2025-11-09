bc470d193724ce3eba830558f242bf0f6f98889e
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-09 15:23:29 -0500

    feat: implement createCheckoutSession endpoint with JWT authentication and Swagger documentation

--- src/subscriptions/presentation/controllers/subscriptions.controller.ts
+++ src/subscriptions/presentation/controllers/subscriptions.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseEnumPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { CurrentUser } from 'src/auth/infrastructure/decorators/current-user.decorator';
+import { JwtAuthGuard } from 'src/auth/infrastructure/guards/jwt.guard';
+import { CreateCheckoutSessionUseCase } from 'src/subscriptions/application/use-cases/create-checkout-session.usecase';
+import { SubscriptionPlan } from 'src/subscriptions/domain/value-objects/subscription-plan.vo';
+import { UserResponseDTO } from 'src/users/application/dto/user-response.dto';
+import { ApiCheckoutSessionEndpoint } from '../decorators/subscriptions.decoratos';
 
 @Controller('subscriptions')
-export class SubscriptionsController {}
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+export class SubscriptionsController {
+  constructor(
+    private readonly createCheckoutSessionUseCase: CreateCheckoutSessionUseCase,
+  ) {}
+
+  @Get('checkout-session/:plan')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCheckoutSessionEndpoint()
+  async createCheckoutSession(
+    @Param('plan', new ParseEnumPipe(SubscriptionPlan)) plan: SubscriptionPlan,
+    @CurrentUser()
+    user: UserResponseDTO,
+  ) {
+    return this.createCheckoutSessionUseCase.execute(user.id, plan);
+  }
+}
