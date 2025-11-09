e552ac81fdc87b9d7248d9986e291c86181c8e30
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-09 17:05:50 -0500

    feat: add static file serving for client assets and create cancel/success HTML pages

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(DataSourceConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     UsersModule,
     AuthModule,
     SharedModule,
