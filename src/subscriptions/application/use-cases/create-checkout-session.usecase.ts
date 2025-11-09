6a639083b416ffe0830bd0a00cc38ae91574f839
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-09 14:54:26 -0500

    feat: add CreateCheckoutSessionUseCase for handling Stripe checkout sessions

---
+++ src/subscriptions/application/use-cases/create-checkout-session.usecase.ts
@@ -0,0 +1,52 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SubscriptionPlan } from 'src/subscriptions/domain/value-objects/subscription-plan.vo';
+import { StripePricingService } from 'src/subscriptions/infrastructure/services/stripe-princing.service';
+import { StripeService } from 'src/subscriptions/infrastructure/services/stripe.service';
+
+import { UserRepository } from 'src/users/domain/repositories/user.repository';
+
+@Injectable()
+export class CreateCheckoutSessionUseCase {
+  constructor(
+    @Inject('UserRepository')
+    private userRepository: UserRepository,
+    private readonly stripeService: StripeService,
+    private readonly stripePricingService: StripePricingService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async execute(userId: string, plan: SubscriptionPlan) {
+    const user = await this.userRepository.findById(userId);
+
+    if (!user) {
+      throw new NotFoundException(`User with the ID ${userId} was not found`);
+    }
+
+    let customerId = user.stripeCustomerId;
+
+    if (!customerId) {
+      customerId = await this.stripeService.createCustomer(
+        user.email,
+        user.name,
+      );
+
+      user.stripeCustomerId = customerId;
+      await this.userRepository.save(user);
+    }
+
+    const priceId = this.stripePricingService.getPlanPriceId(plan);
+
+    const session = await this.stripeService.createCheckoutSession({
+      customerId,
+      priceId,
+      successUrl: this.configService.getOrThrow<string>('SUCCESS_PAYMENT_URL'),
+      cancelUrl: this.configService.getOrThrow<string>('CANCEL_PAYMENT_URL'),
+    });
+
+    return {
+      sessionId: session.id,
+      checkoutUrl: session.url,
+    };
+  }
+}
