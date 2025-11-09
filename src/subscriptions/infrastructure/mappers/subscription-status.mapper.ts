6eff991105b845fdd60e504918f136e5e313d682
Alejandro Moreno <alejandromogil@gmail.com>
2025-11-09 18:21:30 -0500

    feat: refactor subscription status enum and implement mapper for Stripe status

---
+++ src/subscriptions/infrastructure/mappers/subscription-status.mapper.ts
@@ -0,0 +1,28 @@
+import { SubscriptionStatus } from 'src/subscriptions/domain/value-objects/subscription-status.vo';
+import Stripe from 'stripe';
+
+export class SubscriptionStatusMapper {
+  static fromStripe(status: Stripe.Subscription.Status): SubscriptionStatus {
+    switch (status) {
+      case 'active':
+        return SubscriptionStatus.ACTIVE;
+
+      case 'canceled':
+        return SubscriptionStatus.CANCELED;
+
+      case 'incomplete':
+      case 'unpaid':
+        return SubscriptionStatus.INCOMPLETE;
+
+      case 'incomplete_expired':
+      case 'past_due':
+        return SubscriptionStatus.EXPIRED;
+
+      default:
+        throw new Error(
+          "The stripe status value doesn't have an implemented mapper: " +
+            status,
+        );
+    }
+  }
+}
